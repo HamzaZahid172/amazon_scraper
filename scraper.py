eb6a26b2d44dd46037cee4109cc2f5f728fd32f0
Hamza Zahid Butt <hamzazahidbutt@wlan-c1234.fem.tu-ilmenau.de>
2026-02-07 19:31:50 +0100

    feat: update the output file format and screenshot after get the result from search and detail page

--- scraper.py
+++ scraper.py
@@ -41,6 +41,7 @@ async def extract_search_result(page: Page, search_url: str) -> Optional[Dict]:
             if (!container) return null;
 
             const titleEl = container.querySelector("h2 span");
+            const searchResults = document.querySelectorAll("div[role='listitem'][data-asin]").length;
             const priceEl = Array.from(container.querySelectorAll("div[class='puisg-col-inner']"))
                     .find(x => x.innerText.toLowerCase().includes(`${"paperback"|"kindle"|"hardcover"|"audiobook"}`));
 
@@ -51,7 +52,8 @@ async def extract_search_result(page: Page, search_url: str) -> Optional[Dict]:
                 title: titleEl ? titleEl.innerText.trim() : null,
                 url: url,
                 asin: url.includes("/dp/") ? url.split("/dp/")[1].split("/")[0] : null,
-                searchPrice: priceEl ? priceEl.innerText.trim() : null
+                searchPrice: priceEl ? priceEl.innerText.trim() : null,
+                searchResults: searchResults
             };
         }
     """, search_url)
@@ -82,6 +84,13 @@ async def extract_search_result(page: Page, search_url: str) -> Optional[Dict]:
             return None 
 
         return "$" + price.group(1)
+    
+    search_screenshot_path = f"output/screenshots/search/{item['asin']}.png"
+
+    await page.screenshot(
+        path=search_screenshot_path,
+        full_page=True
+    )
 
     return {
         "Search URL": item["searchUrl"],
@@ -91,17 +100,19 @@ async def extract_search_result(page: Page, search_url: str) -> Optional[Dict]:
         "Paperback Price": getPrice(item["searchPrice"], "paperback"),
         "Hardcover Price": getPrice(item["searchPrice"], "hardcover"),
         "Kindle Price": getPrice(item["searchPrice"], "kindle"),
-        "AudioBook Price": getPrice(item["searchPrice"], "audiobook")
+        "AudioBook Price": getPrice(item["searchPrice"], "audiobook"),
+        "searchResults": item["searchResults"],
+        "Search Screenshot": search_screenshot_path
     }
 
-async def extract_product(page: Page, search_data: Dict) -> Dict:
-    asin = search_data["ASIN"]
+async def extract_product(page: Page, getSearchData: Dict, inputRowValue: pd.Series) -> Dict:
+    asin = getSearchData["ASIN"]
     url = f"https://www.amazon.com/dp/{asin}"
 
     await page.goto(url, timeout=60_000, wait_until="domcontentloaded")
     await page.wait_for_selector("#tmmSwatches", timeout=15000)
 
-    title = await page.locator("#productTitle").inner_text()
+    # title = await page.locator("#productTitle").inner_text()
     swatches = await page.locator("#tmmSwatches").inner_text()
 
     def get_price(label):
@@ -113,21 +124,30 @@ async def extract_product(page: Page, search_data: Dict) -> Dict:
         m = re.search(r"\$\s*(\d+\.\d+)", part)
         return "$" + m.group(1) if m else None
 
+    product_screenshot_path = f"output/screenshots/product/{getSearchData['ASIN']}.png"
+    await page.screenshot(
+        path=product_screenshot_path,
+        full_page=True)
+
     return {
-        "Search URL": search_data["Search URL"],
-        "Search Title": search_data["Title"],
-        "Search ASIN": search_data["ASIN"],
-        "Search Page Paperback Price": search_data["Paperback Price"],
-        "Search Page Hardcover Price": search_data["Hardcover Price"],
-        "Search Page Kindle Price": search_data["Kindle Price"],
-        "Search Page AudioBook Price": search_data["AudioBook Price"],
-        "Product URL": url,
-        "ASIN": asin,
-        "Title": title.strip(),
-        "Paperback Price": get_price("paperback"),
-        "Hardcover Price": get_price("hardcover"),
-        "Kindle Price": get_price("kindle"),
-        "AudioBook Price": get_price("audio"),
+        "Title": inputRowValue["Title"],
+        "ASIN": inputRowValue["ASIN"],
+        "Price": inputRowValue["Price"],
+        "Input url": inputRowValue["Input_url"],
+        "Search url": inputRowValue["SEARCH_URL"],
+        "Paperback Min Price": get_price("paperback"),
+        "Hardcover Min Price": get_price("hardcover"),
+        "Kindle Min Price": get_price("kindle"),
+        "AudioBook Min Price": get_price("audio"),
+        "Product Page Screenshot": product_screenshot_path,
+        "# Search results": getSearchData["searchResults"],
+        "Search Title": getSearchData["Title"],
+        "Search Asin": getSearchData["ASIN"],
+        "Search Paperback Min Price": getSearchData["Paperback Price"],
+        "Search Hardcover Min Price": getSearchData["Hardcover Price"],
+        "Search Kindle Min Price": getSearchData["Kindle Price"],
+        "Search AudioBook Min Price": getSearchData["AudioBook Price"],
+        "Search Result Screenshot": getSearchData["Search Screenshot"],
     }
 
 async def create_browser_context(p) -> BrowserContext:
@@ -150,34 +170,33 @@ async def create_browser_context(p) -> BrowserContext:
 async def run():
     Path("output").mkdir(parents=True, exist_ok=True)
 
-    df = pd.read_excel(INPUT_FILE)
+    inputFile = pd.read_excel(INPUT_FILE)
 
-    if "SEARCH_URL" not in df.columns:
+    if "SEARCH_URL" not in inputFile.columns:
         raise ValueError("Excel must contain SEARCH_URL column")
-
-    search_urls = df["SEARCH_URL"].dropna().tolist()
     results = []
 
     async with async_playwright() as p:
         context = await create_browser_context(p)
         page = await context.new_page()
 
-        for search_url in search_urls:
+        for _, rowValue in inputFile.iterrows():
+            search_url = rowValue["SEARCH_URL"]
             print(f"[INFO] Search: {search_url}")
 
-            search_data = await extract_search_result(page, search_url)
-            if not search_data or not search_data["ASIN"]:
+            getSearchData = await extract_search_result(page, search_url)
+            if not getSearchData or not getSearchData["ASIN"]:
                 print("[WARN] No product found")
                 continue
 
-            print(f"[INFO] Found ASIN: {search_data['ASIN']}")
+            print(f"[INFO] Found ASIN: {getSearchData['ASIN']}")
 
             try:
-                row = await extract_product(page, search_data)
+                row = await extract_product(page, getSearchData, rowValue)
                 results.append(row)
                 await asyncio.sleep(random.randint(MIN_DELAY, MAX_DELAY))
             except Exception as e:
-                print(f"[ERROR] {search_data['ASIN']} -> {e}")
+                print(f"[ERROR] {getSearchData['ASIN']} -> {e}")
 
         await context.close()
 
